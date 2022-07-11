872e864375391a7584488a124d342953b02a541b
heureka-code <heureka-code>
2022-07-11 17:38:54 +0200

    feat(main): Erste Version

---
+++ enc_netzwerkverbindung_heureka_code/_generell/verbindung/__init__.py
@@ -0,0 +1,4 @@
+from .abc_verbindung import VerbindungsVerwalter
+from .client_verbindung import (ClientVerbindungsVerwalter,
+                                VerbindungFehlgeschlagen)
+from .server_verbindung import ServerVerbindungsVerwalter
